7ce79151309952e89d7a8942f31f01dff0ec7829
Riya Srivastava <riyasrivastava@Riyas-MacBook-Pro.local>
2021-02-23 13:03:45 +0530

    search app v4 commited

--- src/main/java/com/example/Springdemo1/service/impl/SearchServiceImpl.java
+++ src/main/java/com/example/Springdemo1/service/impl/SearchServiceImpl.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class SearchServiceImpl implements SearchService {
@@ -18,17 +21,54 @@ public class SearchServiceImpl implements SearchService {
     @Override
     public SearchResponsedto getProducts(SearchRequestdto request){
 
-        Map<String, Object> products= searchClient.getProducts(request.getSearchTerm());
-        Map<String, Object> location= searchClient.getProducts("q="+request.getLocation());
-
-        List<LinkedHashMap<String, Object>> l=(List<LinkedHashMap<String, Object>>) ((Map)products.get("response")).get("docs");
-        List<LinkedHashMap<String, Object>> m=(List<LinkedHashMap<String, Object>>) ((Map)location.get("response")).get("docs");
-
         SearchResponsedto responsedto= new SearchResponsedto();
-
-        List<Productdto> product1= new ArrayList<>();
+        Map<String, Object> location= searchClient.getProducts("stockLocation"+request.getLocation());
+        List<LinkedHashMap<String, Object>> m=(List<LinkedHashMap<String, Object>>) ((Map)location.get("response")).get("docs");
         List<Productdto> product2= new ArrayList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(2);
 
+        Runnable task1=() ->{
+            System.out.println(Thread.currentThread().getId());
+            List<Productdto> product1 =getStringObjectMap(request.getSearchTerm());
+            responsedto.setProducts(product1);
+        };
+
+
+
+     Runnable task2=() ->{
+         System.out.println(Thread.currentThread().getId());
+         for(LinkedHashMap<String,Object> k:m){
+             Productdto productdto= new Productdto();
+             productdto.setDescription((String) k.get("description"));
+             productdto.setInstock((int) k.get("isInStock") == 1? true: false );
+             productdto.setTitle((String)k.get("nameSearch") );
+             productdto.setSalePrice(((Double)k.get("offerPrice")).intValue());
+             product2.add(productdto);
+         }
+
+         responsedto.setLocationBasedProducts(product2);
+
+     };
+        executor.execute(task1);
+        executor.execute(task2);
+        executor.shutdown();
+        try {
+            executor.awaitTermination(60, TimeUnit.SECONDS);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+
+
+
+        return responsedto;
+    }
+
+    private List<Productdto> getStringObjectMap(String query) {
+        Map<String, Object> products= searchClient.getProducts(query);
+        List<LinkedHashMap<String, Object>> l=(List<LinkedHashMap<String, Object>>) ((Map)products.get("response")).get("docs");
+        List<Productdto> product1= new ArrayList<>();
         for(LinkedHashMap<String,Object> k:l){
             Productdto productdto= new Productdto();
             productdto.setDescription((String) k.get("description"));
@@ -37,20 +77,6 @@ public class SearchServiceImpl implements SearchService {
             productdto.setSalePrice(((Double)k.get("offerPrice")).intValue());
             product1.add(productdto);
         }
-
-        for(LinkedHashMap<String,Object> k:m){
-            Productdto productdto= new Productdto();
-            productdto.setDescription((String) k.get("description"));
-            productdto.setInstock((int) k.get("isInStock") == 1? true: false );
-            productdto.setTitle((String)k.get("nameSearch") );
-            productdto.setSalePrice(((Double)k.get("offerPrice")).intValue());
-            product2.add(productdto);
-        }
-
-
-
-        responsedto.setProducts(product1);
-        responsedto.setLocationBasedProducts(product2);
-        return responsedto;
+        return product1;
     }
 }
